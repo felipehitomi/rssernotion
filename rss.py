64a0d9f1984892c89e1d7e45069bfa1f5917ceeb
felipe hitomi <felipe.hitomi@gmail.com>
2021-06-16 18:06:46 -0300

    Adicionando features e arquivos de teste.

--- rss.py
+++ rss.py
@@ -1,41 +1,38 @@
 import feedparser
+from html.parser import HTMLParser
+from notion.client import NotionClient
+from notion.block import TextBlock
 
-feed = feedparser.parse("http://feeds.dzone.com/devops")
-entry = feed.entries[0]
+import html2text
 
-print("Title: ",entry.title)
+client = NotionClient(token_v2="f4678525ad72d98f7c312e90e11d22d94be41a812bc1a4922b03dc14307bde3d6fce405dba23e719469c4136885dbb41b6b1aa810e2d02bffc9263dc1d2b4cbc6527b5951af2d17c2b79d493ac4d")
+page = client.get_block("https://www.notion.so/DevOps-RSS-89c60de0b68f4fda91bd13a756e08a1f")
+
+class MyHTMLParser(HTMLParser):
+    def handle_data(self, data):
+        print(data)
 
 
 
-curl 'https://api.notion.com/v1/pages/89c60de0b68f4fda91bd13a756e08a1f' -H 'Notion-Version: 2021-05-13' -H 'Authorization: Bearer '"$NOTION_API_KEY"''
-curl 'https://api.notion.com/v1/blocks/89c60de0b68f4fda91bd13a756e08a1f/children?page_size=100' -H 'Authorization: Bearer '"$NOTION_API_KEY"'' -H "Notion-Version: 2021-05-13"
-curl -X PATCH 'https://api.notion.com/v1/blocks/89c60de0b68f4fda91bd13a756e08a1f/children' \
-  -H 'Authorization: Bearer '"$NOTION_API_KEY"'' \
-  -H "Content-Type: application/json" \
-  -H "Notion-Version: 2021-05-13" \
-  --data '{
-	"children": [
-		{
-			"object": "block",
-			"type": "heading_2",
-			"heading_2": {
-				"text": [{ "type": "text", "text": { "content": "Lacinato kale" } }]
-			}
-		},
-		{
-			"object": "block",
-			"type": "paragraph",
-			"paragraph": {
-				"text": [
-					{
-						"type": "text",
-						"text": {
-							"content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
-							"link": { "url": "https://en.wikipedia.org/wiki/Lacinato_kale" }
-						}
-					}
-				]
-			}
-		}
-	]
-}'
+def rssparser(url):
+    parser = MyHTMLParser()
+    feed = feedparser.parse(url)
+    for entry in feed.entries:
+        title = entry.title
+        link = entry.link
+        text = html2text.html2text(entry.summary)
+        write_notion(title,link,text)
+
+def link_finder(file):
+    f = open(file, "r")
+    for param in f.readlines():
+        rssparser(param)
+
+def write_notion(title,link,text):
+    page.children.add_new(TextBlock, title=title)
+    page.children.add_new(TextBlock, title=link)
+    page.children.add_new(TextBlock, title=text)
+
+
+if __name__ == "__main__":
+    link_finder("links.txt")
